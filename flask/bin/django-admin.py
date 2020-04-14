f0fff9dd6c1f843668af24a574282358baf774cc
Pascual <pascual.vila.segura@hotmail.com>
2020-04-14 12:37:52 +0100

    Flask Social Blog last commits

---
+++ flask/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/pascual/Documents/Estudios/Flask-Bootcamp-master-Udemy/SocialBlogApp/flask/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
